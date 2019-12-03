423bc1fdb179d0754a0037fe0befb59b250fc620
Cameron Asbury <CameronAsbury@openarc.net>
2019-12-03 11:52:18 -0500

    Added code to read in input.txt into a list

--- AdventOfCodeDay2/Program.cs
+++ AdventOfCodeDay2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace AdventOfCodeDay2
 {
@@ -6,6 +8,25 @@ namespace AdventOfCodeDay2
     {
         static void Main(string[] args)
         {
+            string file = "input.txt";
+            List<string> opcodes = new List<string>();
+
+            using (var reader = new StreamReader(file))
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                for (int i = 0;  i < values.Length; i++)
+                {
+                    opcodes.Add(values[i]);
+                }
+            }
+
+            foreach (string i in opcodes)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
+
